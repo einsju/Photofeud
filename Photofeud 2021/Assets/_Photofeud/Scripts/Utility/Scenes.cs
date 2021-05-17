6ceb9363c116b102a5c59430588bbda2f02db31e
einsju <einsju@hotmail.com>
2021-05-17 13:55:36 +0200

    Just before removing scene "Sign in With Email"

--- Photofeud 2021/Assets/_Photofeud/Scripts/Utility/Scenes.cs
+++ Photofeud 2021/Assets/_Photofeud/Scripts/Utility/Scenes.cs
@@ -3,7 +3,7 @@ namespace Photofeud.Utility
     public class Scenes
     {
         public const string Game = "Game";
-        public const string Main = "Home";
+        public const string Main = "Main";
         public const string Leaderboard = "Leaderboard";
         public const string Login = "Sign In";
     }
